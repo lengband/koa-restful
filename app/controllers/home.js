49fcf5149b9b8b2474cf09b85274e9968a0536a6
wangpeng <email@qq.com>
2019-09-17 22:33:19 +0800

    fix bug

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
 class HomeCtl {
-  index () {
-    changeExt.body = '<h1>这是主页</h1>'
+  index (ctx) {
+    ctx.body = '<h1>这是主页</h1>'
   }
   upload (ctx) {
     const file = ctx.request.files.file
